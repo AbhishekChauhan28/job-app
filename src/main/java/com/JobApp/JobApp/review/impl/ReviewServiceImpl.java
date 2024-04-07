595e8be86d1714818ec0d754cc64cf6da159376e
AbhishekChauhan28 <chauhanabhi280@gmail.com>
2024-04-07 21:18:12 +0530

    fourth commit

---
+++ src/main/java/com/JobApp/JobApp/review/impl/ReviewServiceImpl.java
@@ -0,0 +1,9 @@
+package com.JobApp.JobApp.review.impl;
+
+import com.JobApp.JobApp.review.ReviewService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+}
